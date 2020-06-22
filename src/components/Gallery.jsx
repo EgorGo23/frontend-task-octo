61ed8e1e9356cb8b77d9deabaea981dc6fc2504c
Egor <egor.go2303@gmail.com>
2020-06-22 22:00:31 +0300

    gallery last item

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect, useState, useLayoutEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import * as actions from '../actions/actions';
-import getFormattedImageSrcs from '../helpers/getFormattedImageSrcs';
+import getFormattedImageSrc from '../helpers/getFormattedImageSrc';
 import howManyElemsFitInBlock from '../helpers/howManyElemsFitInBlock';
+import Modal from './Modal';
 
 const GalleryContainer = styled.div`
     ${(props) => props.styles || ''};
@@ -19,38 +20,24 @@ const GalleryContainer = styled.div`
     }
 `;
 
-const Img = styled.img`
-    ${props => props.styles || ''}
-    border-radius: 8px;
-    
+const GalleryItem = styled.li`
+    ${props => props.styles}
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
 `;
 
-const Modal = styled.div`
-    position: fixed; /* фиксированное положение */
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: rgba(0,0,0,0.5); /* цвет фона */
-    z-index: 1050;
-    margin: 0;
-    padding: 0;
+const Img = styled.img`
+    border-radius: 8px;
+`;
+
+const LastItemWrapper = styled.div`
+    position: absolute;
     width: 100%;
     height: 100%;
-
-    & img {
-        width: 50%;
-        height: 55%; 
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        border-radius: 8px;
-    }
-`;
-
-const RestPhoto = styled.button`
-
+    background: rgba(0.51, 0.51, 0.51, 0.5);
+    border-radius: 8px;
 `;
 
 const mapStateToProps = state => {
@@ -66,32 +53,36 @@ const actionCreators = {
 
 const Gallery = (props) => {
     const galleryRef = useRef();
-    const [galleryDimensions, setDimensions] = useState(null);
+    const imgRef = useRef();
+    const [galleryDimensions, setGalleryDimensions] = useState(null);
+    const [imgDimensions, setImgDimensions] = useState(null);
     
     useEffect(() => {
-        setDimensions(galleryRef.current.offsetWidth);
+        setGalleryDimensions(galleryRef.current.offsetWidth);
+        //setImgDimensions(imgRef.current.offsetWidth);
+        console.log(galleryRef.current);
     }, [])
-
+    
     const openImage = ({ target }) => {
         props.selectImage(target.src);
     }
 
-    const formattedSrcs = props.images.map((imgUrl) => {
-        const imgName = imgUrl.match(/\d.*/)[0];
-        const template = 'https://test.octweb.ru/api/crop/media/uploads/gallery/gallery/';
-
-        return template + `${imgName}` + '?geometry=' + `${parseInt(props.styles.imgElem.width, 10)}x${parseInt(props.styles.imgElem.height, 10)}`
-    });
-    
+    const formattedSrcs = props.images.map((imgUrl) => getFormattedImageSrc(imgUrl, props.styles.galleryElm));
     
     return (
         <GalleryContainer styles={props.styles.galleryContainer} ref={galleryRef} >
             <ul>
                 {
-                    formattedSrcs.slice(0, howManyElemsFitInBlock(galleryDimensions,props.styles.imgElem.width)).map((imgUrl) => (
-                        <li key={imgUrl} onClick={(e) => openImage(e)}>
-                            <Img src={imgUrl} styles={props.styles.imgElem} />
-                        </li>
+                    formattedSrcs.slice(0, howManyElemsFitInBlock(galleryDimensions, props.styles.galleryElm.width)).map((imgUrl, ind, array) => (
+                        <GalleryItem key={imgUrl} onClick={(e) => openImage(e)} styles={props.styles.galleryElm}>
+                                <Img src={imgUrl} ref={imgRef} />
+                                {
+                                    ind === array.length - 1 &&
+                                    (
+                                        <LastItemWrapper />
+                                    )
+                                }
+                        </GalleryItem>
                     ))
                 }
             </ul>
@@ -100,7 +91,9 @@ const Gallery = (props) => {
                 props.imgInModalSrc
                 && (
                     <Modal>
-                        <img src={props.imgInModalSrc} />
+                        <div>
+                            <img src={props.imgInModalSrc} />
+                        </div>
                     </Modal>
                 )
             }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,77 @@
+import React, { useRef, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+import isEmpty from '../helpers/isEmpty';
+
+const ModalContainer = styled.div`
+    position: fixed; 
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgba(0,0,0,0.5);
+    z-index: 1050;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & > div {
+        width: 40%;
+        height: 60%;
+        position: relative;
+
+        & img {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            border-radius: 8px;
+        }
+    }    
+`;
+
+const mapStateToProps = state => {
+    const props = {
+        imgInModalSrc: state.imgInModalSrc,
+    }
+    return props;
+};
+
+const actionCreators = {
+
+}
+
+
+const Modal = (props) => {
+    const wrapperRef = useRef();
+    const [wrapperDimensions, setWrapperDimensions] = useState({});
+    
+    useEffect(() => {
+        setWrapperDimensions({
+            width: wrapperRef.current.offsetWidth,
+            height: wrapperRef.current.offsetHeight,
+        });
+    }, [])
+    
+    const transformSrc = (url) => {
+        const srcWithoutSize = url.match(/.*geometry=/)[0];
+
+        if (!isEmpty(wrapperDimensions)) {
+            return `${srcWithoutSize}${wrapperDimensions.width}x${wrapperDimensions.height}`;
+        }
+    }
+
+    return (
+        <ModalContainer>
+            <div ref={wrapperRef}>
+                <img src={isEmpty(wrapperDimensions) ? '' : transformSrc(props.imgInModalSrc)} />
+            </div>
+        </ModalContainer>
+    )
+}
+
+export default connect(mapStateToProps, actionCreators)(Modal);
